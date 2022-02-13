0cd084af3d5a54ff76892daf945adda337351204
h3yon <khykhy1006@gmail.com>
2022-02-13 17:24:45 +0900

    챕터2 opereator - sum 기능 Operator

--- reactive/src/main/java/com/example/reactive/chapter02/PubSub.java
+++ reactive/src/main/java/com/example/reactive/chapter02/PubSub.java
@@ -1,7 +1,5 @@
 package com.example.reactive.chapter02;
 
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -32,34 +30,42 @@ public class PubSub {
     public static void main(String[] args) throws InterruptedException {
 
         Publisher<Integer> pub = iterPub(Stream.iterate(1, a -> a + 1).limit(10).collect(Collectors.toList()));
-        Publisher<Integer> mapPub = mapPub(pub, s -> s * 10); //얘도 퍼블리셔
-        Publisher<Integer> mapPub2 = mapPub(pub, s -> s * -s); // publisher를 하나 더 만들어서 operator할 수 있음
-        mapPub2.subscribe(logSub());
+//        Publisher<Integer> mapPub = mapPub(pub, s -> s * 10); //얘도 퍼블리셔
+//        Publisher<Integer> mapPub2 = mapPub(pub, s -> s * -s); // publisher를 하나 더 만들어서 operator할 수 있음
+
+        Publisher<Integer> sumPub = sumPub(pub);
+        sumPub.subscribe(logSub());
+    }
+
+    private static Publisher<Integer> sumPub(Publisher<Integer> pub) {
+        return new Publisher<Integer>() {
+            @Override
+            public void subscribe(Subscriber<? super Integer> sub) {
+                pub.subscribe(new DelegateSub(sub){
+                    int sum = 0;
+
+                    @Override
+                    public void onNext(Integer i) {
+                        sum += i;
+                    }
+                    @Override
+                    public void onComplete() {
+                        sub.onNext(sum);
+                        sub.onComplete();
+                    }
+                });
+            }
+        };
     }
 
     private static Publisher<Integer> mapPub(Publisher<Integer> pub, Function<Integer, Integer> f) {
         return new Publisher<Integer>(){
             @Override
             public void subscribe(Subscriber<? super Integer> sub) {
-                pub.subscribe(new Subscriber<Integer>() {
-                    @Override
-                    public void onSubscribe(Subscription s) { //받아저 작업하고 logSubs의 subscriber한테 줌
-                        sub.onSubscribe(s); //중개만 해줌
-                    }
-
+                pub.subscribe(new DelegateSub(sub){
                     @Override
                     public void onNext(Integer i) {
-                        sub.onNext(f.apply(i)); //function을 적용시킴
-                    }
-
-                    @Override
-                    public void onError(Throwable t) {
-                        sub.onError(t);
-                    }
-
-                    @Override
-                    public void onComplete() {
-                        sub.onComplete();
+                        super.onNext(f.apply(i));
                     }
                 });
             }
